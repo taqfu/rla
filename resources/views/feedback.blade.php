d821eed9d5ff938969f3e324899eeeedc873cfbd
taqfu <2buzesn@gmail.com>
2016-06-01 14:37:30 -0400

    Added link to reddit

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -2,12 +2,18 @@
 @extends('layouts.app')
 @section('content')
 <div class='center'>
+<p>
     Hey, so it's me, <a href='http://taqfu.com'>taqfu</a>. Just let me know what you think could be better or if there are any issues. Message me 
 @if (Auth::user())
    <a href="{{route('new_message', ['id'=>1])}}">here</a>  or 
 @endif
 <a href="mailto::taqfu0@gmail.com">here.</a>
+</p>
+<p>
+    You can also comment on Reddit <a href='https://www.reddit.com/r/doitproveit/'>here</a>.
+</p>
 </div>
+
 @endsection
 
 
