41feb52a79804798c210a958dc46e355d92e226f
taqfu <2buzesn@gmail.com>
2016-06-01 14:19:18 -0400

    Added discussion count and changed format for messages.

--- resources/views/Achievement/menu.blade.php
+++ resources/views/Achievement/menu.blade.php
@@ -1,7 +1,10 @@
+<?php
+    use App\Comment;
+?>
 <nav class='menu'>
     @if ($active_item=='info')
         <strong>Info</strong>
-        <a href="{{route('discussion', ['id'=>$id])}}">Discussion</a>
+        <a href="{{route('discussion', ['id'=>$id])}}">Discussion ({{count(Comment::where('achievement_id', $id)->get())}})</a>
     @elseif ($active_item=='discussion')
         <a href="{{route('achievement.show', ['id'=>$id])}}">Info</a>
         <strong>Discussion</strong>
