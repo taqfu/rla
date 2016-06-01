5f1854bb14d39549c1cd5a685aad5258d0982d1e
taqfu <2buzesn@gmail.com>
2016-06-01 14:42:40 -0400

    Edited feedback message

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class='center'>
 <p>
-    Hey, so it's me, <a href='http://taqfu.com'>taqfu</a>. Just let me know what you think could be better or if there are any issues. Message me 
+   Hey, so it's me, <a href='http://taqfu.com'>taqfu</a>. Let me if there are any issues or if you think something could be better. Message me 
 @if (Auth::user())
    <a href="{{route('new_message', ['id'=>1])}}">here</a>  or 
 @endif
 <a href="mailto::taqfu0@gmail.com">here.</a>
 </p>
 <p>
-    You can also comment on Reddit <a href='https://www.reddit.com/r/doitproveit/'>here</a>.
+    You can also comment on our subreddit <a href='https://www.reddit.com/r/doitproveit/'>here</a>.
 </p>
 </div>
 
