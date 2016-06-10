85536c79a4196d0da6c091bf86d19b8b239dab3f
taqfu <2buzesn@gmail.com>
2016-06-10 15:51:54 -0400

    Updated changelist

--- resources/views/changes.blade.php
+++ resources/views/changes.blade.php
@@ -4,6 +4,10 @@
 <p>
     <i>This is where you'll be able to see the changes I've made.</i> <a href="{{route('feedback')}}">Head here for any feedback.</a>
 </p>
+<h4>06/10/16</h4>
+<ul>
+    <li>Hopefully made the filters a little clearer by changing the caption names and also adding a (?) tooltip to it.</li>
+</ul>
 <h4>06/09/16</h4>
 <ul>
     <li>Added voting to achievements. Thanks <a href="https://www.reddit.com/user/bobyd">/u/bobyd</a>!</li>
