e52dbd126eebb13bbec5953ed3ee5dd0295e0358
taqfu <2buzesn@gmail.com>
2016-06-22 21:30:36 -0400

    Accidentally deleted images

---
+++ database/migrations/2016_06_08_193401_change-created-by-to-user-id-for-achievements.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeCreatedByToUserIdForAchievements extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('achievements', function (Blueprint $table) {
+            $table->renameColumn('created_by', 'user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('achievements', function (Blueprint $table) {
+            //
+        });
+    }
+}
