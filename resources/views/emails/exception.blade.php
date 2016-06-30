e3b5809edc71404a8827f913046449ecd45d0bac
taqfu <2buzesn@gmail.com>
2016-06-30 15:44:04 -0400

    Changed format of error email.

--- resources/views/emails/exception.blade.php
+++ resources/views/emails/exception.blade.php
@@ -1,6 +1,5 @@
 <h1>{{$user}}</h1>
-<h2>Request</h2>
-<p>From {{$prev}} to {{$url}} - {{$ip}}</p>
+<p>{{url}} from {{$prev}} - {{$ip}}</p>
 <p>{{var_dump($all_requests)}}</p>
 <h2>Error</h2>
 <pre>{{$error}}</pre>
